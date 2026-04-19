95c10d664966308d52d17e81245873cce0840c42
lisandrojm <lisandrojm@gmail.com>
2026-04-19 06:58:22 -0300

    frontend-start-clean

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => {
         mode === "development"
           ? {
               "/api": {
-                target: "http://localhost:8080", // backend local
+                target: "http://127.0.0.1:8080", // backend local
                 changeOrigin: true,
                 secure: false,
               },
