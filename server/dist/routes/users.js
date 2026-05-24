afa7d8e39b41ed945640c8c69276b6bf71aeaecc
lisandrojm <lisandrojm@gmail.com>
2026-05-24 17:10:25 -0300

    users.js

--- server/dist/routes/users.js
+++ server/dist/routes/users.js
@@ -669,12 +669,14 @@ router.put("/:id/approve-solicitud", requireTenant, authenticateToken, requirePe
             // Check if a UserProject already exists for this combination
             let userProject = await UserProject.findOne({
                 projectId: new Types.ObjectId(projId.toString()),
+                userId: new Types.ObjectId(userId),
                 externalEmployeeId: 0,
                 externalProjectId: 0,
             });
             if (!userProject) {
                 userProject = new UserProject({
                     projectId: new Types.ObjectId(projId.toString()),
+                    userId: new Types.ObjectId(userId),
                     externalProjectId: 0,
                     externalEmployeeId: 0,
                     nombre_proyecto: nombreProyecto,
