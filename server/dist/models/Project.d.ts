1be80a5f1e0e886ae53fc8662c43456ce674f4fd
lisandrojm <lisandrojm@gmail.com>
2026-07-21 17:59:19 -0300

    proyectos-empresa

--- server/dist/models/Project.d.ts
+++ server/dist/models/Project.d.ts
@@ -34,6 +34,8 @@ export interface IWorkSchedule {
 export interface IProject extends Document {
     tenantId: Types.ObjectId;
     clientId?: Types.ObjectId;
+    contratoEmpresa?: Types.ObjectId;
+    releaseEmpresa?: Types.ObjectId;
     name: string;
     description?: string;
     status: "active" | "completed" | "on_hold" | "archived";
